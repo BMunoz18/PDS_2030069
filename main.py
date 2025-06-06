fc8fc1f2216dc17a178990a88ee77c9d0f7590c0
BMunoz18 <brandonbra2001@hotmail.com>
2025-06-06 14:34:46 -0600

    Se subió la actividad 3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from src.u1act1 import signals1
 from src.u1act2 import signals2
+from src.u1act3 import signals3
 
 def main(options):
     if options[1] == "u1act1":
@@ -9,9 +10,14 @@ def main(options):
         if len(options) > 2:
             signals2(options[2])
         else:
-            print("Please give a frequency. Example: python main.py act_2 freq")
+            print("Please give a frequency. Example: python main.py u1act2 freq")
+    elif options[1] == "u1act3":
+        if len(options) > 4:
+            signals3(options[2], options[3], options[4])
+        else:
+            print("Please give amplitude, frequency and phase. Example: python main.py u1act3 amp freq phas")
     else:
-        print("Invalid option. Use 'u1act1', 'u1act12', etc.")
+        print("Invalid option. Use 'u1act1', 'u1act2', 'u1act3', etc.")
 
 if __name__ == '__main__':
     args = sys.argv
@@ -20,4 +26,5 @@ if __name__ == '__main__':
     else:
         print("Please give an argument")
         print("Example (run unit 1 - activity 1 ): python main.py u1act1")
-        print("Example (run unit 2 - activity 1 ): python main.py u1act1")
+        print("Example (run unit 1 - activity 2 ): python main.py u1act2 2")
+        print("Example (run unit 1 - activity 3 ): python main.py u1act3 1 2 0.7854")
